ca53add0ebe2c59d6e27b39f25ddc9df951ab751
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2018-08-17 18:55:58 -0400

    Add a delete eventlistener and the method in the TodoList (parent component)

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -4,7 +4,7 @@
         <p>Complete Tasts: {{todos.filter(todo => {return todo.done == true}).length}}</p>
         <p>Pending Tasts: {{todos.filter(todo => {return todo.done == false}).length}}</p>
         <!-- We are now passing data to the child component "Todo" to render the todo list -->
-        <todo v-for="todo in todos" :key="todo.null" v-bind:todo="todo"></todo>
+        <todo v-on:delete-todo="deleteTodo" v-for="todo in todos" :key="todo.null" v-bind:todo="todo"></todo>
         
     </div>
 </template>
@@ -15,6 +15,12 @@ export default {
     props:['todos'],
     components: {
         Todo,
+    },
+    methods: {
+        deleteTodo(todo){
+            const todoIndex = this.todos.indexOf(todo);
+            this.todos.splice(todoIndex,1); 
+        },
     },
 };
 </script>
