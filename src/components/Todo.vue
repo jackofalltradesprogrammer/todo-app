98c0f5106a5d87ec94c710818083579ff84b1162
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2018-08-17 20:41:01 -0400

    Adding Complete todo functionality

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -41,7 +41,7 @@
             <div class ="ui bottom attached green basic button" v-show="!isEditing && todo.done" disabled>
                 Completed
             </div>
-            <div class ="ui bottom attached red basic button" v-show="!isEditing && !todo.done">
+            <div class ="ui bottom attached red basic button" v-show="!isEditing && !todo.done" v-on:click="completeTodo(todo)">
                 Pending
             </div>
         </div>
@@ -67,6 +67,9 @@
 
             deleteTodo(todo){
                 this.$emit('delete-todo', todo);
+            },
+            completeTodo(todo){
+                this.$emit('complete-todo', todo);
             },
         },
     };
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -4,7 +4,7 @@
         <p>Complete Tasts: {{todos.filter(todo => {return todo.done == true}).length}}</p>
         <p>Pending Tasts: {{todos.filter(todo => {return todo.done == false}).length}}</p>
         <!-- We are now passing data to the child component "Todo" to render the todo list -->
-        <todo v-on:delete-todo="deleteTodo" v-for="todo in todos" :key="todo.null" v-bind:todo="todo"></todo>
+        <todo v-on:complete-todo="completeTodo" v-on:delete-todo="deleteTodo" v-for="todo in todos" :key="todo.null" v-bind:todo="todo"></todo>
         
     </div>
 </template>
@@ -20,6 +20,10 @@ export default {
         deleteTodo(todo){
             const todoIndex = this.todos.indexOf(todo);
             this.todos.splice(todoIndex,1); 
+        },
+        completeTodo(todo){
+            const todoIndex = this.todos.indexOf(todo);
+            this.todos[todoIndex].done=true; 
         },
     },
 };
